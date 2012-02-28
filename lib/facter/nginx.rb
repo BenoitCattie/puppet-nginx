89b989fc9b3a2bb771898bb3ac89abfb44e5dffb
Lars Fronius <lars@jimdo.com>
2012-02-28 11:34:11 +0100

    Makes fact more generic usable if you want to read out other nginx config stuff

--- lib/facter/nginx.rb
+++ lib/facter/nginx.rb
@@ -1,14 +1,16 @@
-Facter.add(:nginxversion) do
-    setcode do
-        pattern = %r{^\d{1,}\.\d{1,}\.\d{1,}$}
-        unless defined?(@@nginxversion)
-            @@nginxversion = Facter::Util::Resolution.exec('nginx -v 2>&1 | cut -d "/" -f 2')
-        end
+{  "nginxversion"         => %r{^nginx version: nginx\/(\d{1,}\.\d{1,}\.\d{1,})$},
+}.each do |fact, pattern|
+    Facter.add(fact) do
+        setcode do
+            unless defined?(@@nginxversion)
+                @@nginxversion = Facter::Util::Resolution.exec('nginx -v 2>&1')
+            end
 
-        if pattern.match(@@nginxversion)
-            @@nginxversion
-        else
-            nil
+            if pattern.match(@@nginxversion)
+                $1
+            else
+                nil
+            end
         end
     end
 end
